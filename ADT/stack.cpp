b83d5eec0ea192c92e8e6aba45a125179df33118
DrakenWan <kartikaykaul13@gmail.com>
2019-06-18 14:16:13 +0530

    Stack updated. Working fine

--- ADT/stack.cpp
+++ ADT/stack.cpp
@@ -1,18 +1,26 @@
-// stack implementation
+/*
+/////////////////// STACK IMPLEMENTATION //////////////////////
+Date: 6/18/2019
+Author: Kartikay Kaul
+
+////////////////// +++++++++++++++++++++ //////////////////////
+ */
 #include<iostream>
 #include<vector>
 
 using namespace std;
 
-template<typename S>
+template <typename S>
 class stack {
     vector<S> content;
+    int length;
     public:
         stack() {
+            length = content.size();
         }
         stack(S value)
         {
-            this.push_back(value)
+            content.push_back(value);
         }
 
         void push(S value);
@@ -20,12 +28,48 @@ class stack {
         S peek();
 };
 
-template<typename S>
-void stack::push(S value)
+template <typename S>
+void stack<S>::push(S value)
 {
- 
+    try {
+        content.push_back(value);
+        length = content.size();
+    } catch(std::exception e)
+    {
+        cout<<"Error! Memory overflow!";
+    }
 }
+
+template <typename S>
+S stack<S>::pop()
+ {
+          if(length != 0)
+          {
+            S value = content.back();
+            content.pop_back();
+            length = content.size(); 
+            return value; 
+          }
+          cout<<"\nStack underflow.\n";
+          return -1;
+
+}
+
+template <typename S>
+ S stack<S>::peek()
+ {
+    return content[length - 1];
+ }
+
 int main()
 {
+    
+    stack<int> s1;
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+    cout<<s1.pop()<<endl;
+    cout<<s1.pop()<<endl;
+    cout<<s1.pop()<<endl;
     return 0;
 }
