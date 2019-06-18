41431c80094c72f3d5e0effb4ccc2ba18dfdf6f1
DrakenWan <kartikaykaul13@gmail.com>
2019-06-18 15:02:39 +0530

    added new methods to stack

--- ADT/stack.cpp
+++ ADT/stack.cpp
@@ -20,10 +20,13 @@ class stack {
         }
         stack(S value)
         {
-            content.push_back(value);
+            this->push(value);
         }
 
         void push(S value);
+        int StackLength() {return content.size();}
+        void clear();
+        void toString();
         S pop();
         S peek();
 };
@@ -31,6 +34,9 @@ class stack {
 template <typename S>
 void stack<S>::push(S value)
 {
+    /*
+        Push above the top of the stack
+     */
     try {
         content.push_back(value);
         length = content.size();
@@ -43,31 +49,83 @@ void stack<S>::push(S value)
 template <typename S>
 S stack<S>::pop()
  {
-          if(length != 0)
-          {
-            S value = content.back();
-            content.pop_back();
-            length = content.size(); 
-            return value; 
-          }
-          cout<<"\nStack underflow.\n";
-          return -1;
+    /*
+        Pop the top element of the stack
+     */
+    if(length != 0)
+    {
+    S value = content.back();
+    content.pop_back();
+    length = content.size(); 
+    return value; 
+    }
+    cout<<"Stack underflow.\n";
+    return -1;
 
 }
 
 template <typename S>
  S stack<S>::peek()
  {
+     /*
+        Peek the top of the stack
+      */
      if(length != 0)
         return content[length - 1];
      else {
-         cout<<"\nStack Empty\n";
+         cout<<"Stack Empty\n";
          return -1;
      }
  }
 
+template <typename S>
+void stack<S>::clear()
+{
+    /*
+        Clear the stack contents
+     */
+
+    if(this->length != 0)
+    {
+        while(this->length != 0)
+        {
+            this->pop();
+        }
+        cout<<"Stack cleared.\n";
+    }
+    else
+    {
+        cout<<"Stack is already clear.\n";
+    }
+    
+}
+
+template <typename S>
+void stack<S>::toString()
+{
+    /*
+        shows string representation of the internal contents
+     */
+
+    cout<<"\n[";
+    for(int i=0; i<length; i++)
+    {
+        if(i != length - 1)
+            cout<<this->content[i]<<"->";
+        else
+            cout<<this->content[i]<<"(top)";
+    }
+    if(this->length == 0) cout<<"(empty)";
+    cout<<"]\n";
+}
+
 int main()
 {
+    /*
+        Run a stack implementation of any datatype below: be it string int or an object instance itself
+        stack<datatype> instancevariablename
+        You can experiment below with different methods of the object instance
+     */
     
     stack<int> s1;
     s1.push(10);
@@ -80,5 +138,12 @@ int main()
     cout<<s1.peek()<<endl;
     s1.push(25);
     cout<<s1.peek();
+    s1.push(35);
+    s1.push(40);
+    s1.toString();
+    s1.clear();
+    s1.peek();
+    s1.clear();
+    s1.toString();
     return 0;
 }
