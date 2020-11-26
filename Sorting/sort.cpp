b32b72b65cf08e9f1129d8b6da5cd4a7392aaaa2
Draken Wan <kartikaykaul13@gmail.com>
2020-11-26 06:08:05 +0530

    Sorting algorithms being added

---
+++ Sorting/sort.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+
+//edit as may suffice
+class Sort {
+        //edit datatype as may suffice
+        vector<int> data;
+        int length;
+
+    public:
+        Sort() {
+            this->length=0;
+            }
+
+        Sort(vector<int> data) {
+            this->data = data;
+            this->length = data.size();
+        }
+        void insertion();
+        void display();
+};
+
+void Sort::display() {
+    int i;
+    for(i=0; i<this->length; i++)
+        cout<<this->data[i]<<endl;
+}
+
+void Sort::insertion() {
+
+    int i, j , temp, length = this->data.size();
+    //cout<<length;
+
+    for(i=1; i<length; i++)
+     {
+         j = i - 1;
+         temp = this->data[i];
+         while(j > -1 && temp < this->data[j]) {
+            this->data[j+1] = this->data[j];
+            j--;
+         }
+         this->data[j+1] = temp;
+     }
+}
+
+
+int main()
+
+{
+    //int a = 10;
+    //cout<<a;
+
+    // init the dummy data vector
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(23);
+    v.push_back(0);
+    v.push_back(8);
+    v.push_back(11);
+    v.push_back(1);
+    v.push_back(-1);
+
+    Sort s(v);
+    s.display();
+    cout<<endl;
+    s.insertion();
+    s.display();
+    return 0;
+}
