9d95a0b7caa60e3c2ee04171d0b92ec4a8bbe38e
Draken Wan <kartikaykaul13@gmail.com>
2022-06-27 17:26:34 +0530

    Update stack.cpp
    made some changes

--- ADT/stack.cpp
+++ ADT/stack.cpp
@@ -56,6 +56,7 @@ S stack<S>::pop()
     if(length != 0)
     {
     S value = content.back();
+    cout<<"Deleted content value: "<<value;
     content.pop_back();
     length = content.size(); 
     return value; 
@@ -133,14 +134,18 @@ int main()
     stack<int> s1;
     s1.push(1);
     cout<<s1.peek();
-
+    s1.push(10);
+    s1.push(-1);
+    s1.toString();
+    
     stack<string> s2;
-    s1.pop();
-    cout<<s2.peek();
-    s2.push("ad");
-    cout<<s2.peek();
-    cout<<s2.pop();
-    cout<<s2.peek();
+    s2.push("neeta");
+    s2.push("sahil");
+    s2.push("ike");
+    s2.push("camila");
+    s2.push("noora");
+    s2.toString();
+    s2.pop();
     s2.toString();
     return 0;
 }
