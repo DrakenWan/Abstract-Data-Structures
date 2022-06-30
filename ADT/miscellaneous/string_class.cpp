75218c53fc2c435aa711d032d3b300f64e8fa9df
Draken Wan <kartikaykaul13@gmail.com>
2022-06-30 06:12:31 +0530

    trying to create my own string class

---
+++ ADT/miscellaneous/string_class.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<stdio.h>
+
+using namespace std;
+
+class String
+{
+	vector<char> value;
+	int size;
+	
+	public:
+	String() {
+		cout<<this->value.size();
+	}
+	
+	String(vector<char> value) {
+		
+	}
+	//operations
+	
+};
+
+int main() {
+	
+	//string s1 = "aash", s2 = "aash";
+	//cout<<s1.compare(s2);
+	
+	String s;
+	
+	return 0;
+}
