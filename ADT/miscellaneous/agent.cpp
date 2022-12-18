7286bc967408673fdde90d444e9ddf26746d96ea
Draken Wan <kartikaykaul13@gmail.com>
2022-12-18 15:47:06 -0800

    expanded Agent class

--- ADT/miscellaneous/agent.cpp
+++ ADT/miscellaneous/agent.cpp
@@ -1,36 +1,77 @@
 #include<iostream>
 #include "point.h"
 
+using namespace std;
+
 class agent {
+    string name;
     point location;
+
+    public:
+    agent() {
+        this->name = "Agent";
+        point loc(0,0);
+        this->location = loc;
+    }
+
+    agent(double x, double y) {
+        this->name = "Agent";
+        point loc(x,y);
+        this->location = loc;
+    }
+
+    agent(string name, double x, double y) {
+        this->name = name;
+        point loc(x,y);
+        this->location = loc;
+    }
+
+    agent(point loc) {
+        this->name = "Agent";
+        this->location = loc;
+    }
+
+    agent(string name, point loc) {
+        this->name = name;
+        this->location = loc;
+    }
+
+
+    string _name() {return this->name;}
+    point _location() {return this->location;}
+
+    void moveNorth(int stride=1) {
+        location.update(location._y() + stride,1);
+    }
+
+    void moveEast(int stride=1) {
+        location.update(location._x() + stride,0);
+    }
+
+    void moveSouth(int stride=1) {
+        location.update(location._y() - stride,1);
+    }
+
+    void moveWest(int stride=1) {
+        location.update(location._x() - stride,0);
+    }
+
+    friend ostream& operator<<(ostream& os,  agent a);
+
 };
+
+
+ostream& operator<<(ostream& os, agent a)
+{
+    os<<"[";
+    os<<"name: "<<a._name()<<", ";
+    os<<"location: ";
+    os<<a._location()<<"]";
+}
 
 int main() {
 
-    point p1, p2(3,-1,1);
-    int x=3;
-
-    cout<<p1+p2; 
-    point p3 = p1 + 1; // adding a scalar to all components of the point
-    cout<<p3.isOrigin(); // is it an origin point?
-
-    cout<<endl;
-    cout<<p3*x; //multiply a scalar to the point
-
-    cout<<p3.distance(p1+1);
-    cout<<endl;
-    cout<<"p1+1 and p3 ";
-    cout<<((p3 == p1+1)? "equal" : "not equal");
-
-    point p4(1, 20, 5);
-
-    cout<<endl;
-    cout<<p4<<endl;
-    p4.update(3, 2);
-    cout<<endl<<p4;
-
-    point p5(1,1,2);
-
-    cout<<endl<<(p4>p5);
+    agent a("Anya", 2, 3);
+    cout<<a;
     return 0;
 }
