d19e123f4cca226ad1fabd6f4bcc019dbb95bfe1
Draken Wan <kartikaykaul13@gmail.com>
2022-12-20 22:01:02 -0800

    changed sort class

--- Sorting/sort.cpp
+++ Sorting/sort.cpp
@@ -3,20 +3,14 @@
 #include<string>
 #include<cstdlib>
 #include<ctime>
-#define DTYPE int
-#define floatGEN 1.1531
+
+
 
 using namespace std;
 
-void swap(int &a, int &b)
- {
-     int temp;
-     temp = a;
-     a = b;
-     b = temp;
- }
 
 //edit as may suffice
+template<typename S>
 class Sort {
     /* Sort algorithms in ascending order of their O() and T()
 
@@ -25,61 +19,91 @@ class Sort {
     */
         //edit datatype as may suffice
         // select data identifiers from standard library
-        vector<DTYPE> data;
-        int length;
+        vector<S> data;
+        long length;
 		
 		
 		// helper functions for sorting algorithms
-		void merge(vector<DTYPE>&);
-		int partition(vector<DTYPE>&, int, int);
-		void quick(vector<DTYPE>&, int, int);
-		
+		void merge(vector<S>&);
+		long partition(vector<S>&, long, long);
+		void quick(vector<S>&, long, long);
+		void swap(S &a, S &b)
+        {
+            S temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+
+        void returnSortedArray() {
+            return this->data;
+        }
+
     public:
         Sort() {
             this->length=0;
             }
 
-        Sort(vector<DTYPE> data) {
+        Sort(vector<S> data) {
             this->data = data;
             this->length = data.size();
         }
         
-        void changeData(vector<DTYPE>);
-		
+
+        void changeData(vector<S>);
+		void changeData(S val, long idx) {
+            if(idx < this->length)
+                this->data[idx] = val;
+        }
+
+        void addData(S val) {
+            this->data.push_back(val);
+            this->length = this->data.size();
+        }
+        
         void reverse() {
-            int i;
+            long i;
             for(i=0; i<length/2; i++)
-                swap(this->data[i], this->data[length - 1 - i]);
+                this->swap(this->data[i], this->data[length - 1 - i]);
         }
 
         // sorting algos better for low n values
-        void insertion(bool);
-        void bubble(bool); //O(n^2)
-        void selection(bool); //O(n^2)
+        void insertion(bool decr=false);
+        void bubble(bool decr=false); //O(n^2)
+        void selection(bool decr=false); //O(n^2)
 
         // sorting algos for high / very high n values
-        void mergeSort(bool); // O(logn) however if decr = true then O(n)
-		void quickSort(bool); //O(logn) however if decr = true then O(n)
+        void mergeSort(bool decr=false); // O(logn) however if decr = true then O(n)
+		void quickSort(bool decr=false); //O(logn) however if decr = true then O(n)
         void display();
 };
 
-void Sort::changeData(vector<DTYPE> data) {
+template<typename S>
+void Sort<S>::changeData(vector<S> data) {
     //update data altogether. No pushbacks to the vector
     this->data = data;
     this->length = data.size();
     //cout<<endl<<"Updated Data."<<endl;
 }
 
-void Sort::display() {
-    int i;
-    for(i=0; i<this->length; i++)
-        cout<<this->data[i]<<endl;
+
+template<typename S>
+void Sort<S>::display() {
+    long i;
+    cout<<"[";
+    for(i=0; i<this->length; i++) {
+        cout<<this->data[i];
+        if(this->length-1 > i) cout<<", ";
+    }
+    cout<<"]";
 }
 
-void Sort::insertion(bool decr=false) {
+template<typename S>
+void Sort<S>::insertion(bool decr) {
 
-    int i, j, length = this->data.size();
-    DTYPE temp;
+    long i, j, length = this->data.size();
+    S temp;
     //cout<<length;
 
     for(i=1; i<length; i++)
@@ -97,23 +121,25 @@ void Sort::insertion(bool decr=false) {
         reverse();
 }
 
-void Sort::bubble(bool decr=false) {
+template<typename S>
+void Sort<S>::bubble(bool decr) {
     //O(n^2)
-    int i, j;
+    long i, j;
     for(i=0; i<this->length - 1; i++)
      {
          for(j=0; j<this->length - 1 - i; j++)
           if (this->data[j] > this->data[j+1])
-            swap(this->data[j], this->data[j+1]);
+            this->swap(this->data[j], this->data[j+1]);
      }
 
      if(decr) //O(n)
         reverse();
 }
 
-void Sort::selection(bool decr=false) {
+template<typename S>
+void Sort<S>::selection(bool decr) {
     //O(n^2)
-    int i, j, min_index;
+    long i, j, min_index;
     for(i=0; i<this->length - 1; i++)
      {
          min_index = i;
@@ -121,7 +147,7 @@ void Sort::selection(bool decr=false) {
           if(this->data[j] < this->data[min_index])
             min_index = j;
          
-         swap(this->data[i], this->data[min_index]);
+         this->swap(this->data[i], this->data[min_index]);
      }
 
       if(decr) //O(n)
@@ -130,9 +156,10 @@ void Sort::selection(bool decr=false) {
 }
 
 
-void Sort::merge(vector<DTYPE> &arr) {
-	int len = arr.size();
-	vector<DTYPE> L, R;
+template<typename S>
+void Sort<S>::merge(vector<S> &arr) {
+	long len = arr.size();
+	vector<S> L, R;
 	if(len > 1) {
 		
 		int mid = len/2;
@@ -179,10 +206,11 @@ void Sort::merge(vector<DTYPE> &arr) {
 	} // OUTERMOST IF CONDITION ENDS
 } //MERGE function
 	
+
+template<typename S>
+void Sort<S>::mergeSort(bool decr) {
 	
-void Sort::mergeSort(bool decr=false) {
-	
-	vector<DTYPE> daa = this->data;
+	vector<S> daa = this->data;
 	
 	merge(daa);
 	
@@ -194,29 +222,31 @@ void Sort::mergeSort(bool decr=false) {
 
 
 // quicksort methods
-int Sort::partition(vector<DTYPE> &arr, int low, int high) {
+template<typename S>
+long Sort<S>::partition(vector<S> &arr, long low, long high) {
 	
-	DTYPE pivot = arr[high];
+	S pivot = arr[high];
 	
-	int i = low - 1;
+	long i = low - 1;
 	
-	for(int j=low; j<high; j++)
+	for(long j=low; j<high; j++)
 		if( arr[j] < pivot) {
 			
 			i++;
-			swap( arr[i], arr[j]);
+			this->swap( arr[i], arr[j]);
 		}
 	
-	swap(arr[i+1], arr[high]);
+	this->swap(arr[i+1], arr[high]);
 	
 	return i+1;
 }
 
-void Sort::quick(vector<DTYPE> &arr, int low, int high) {
+template<typename S>
+void Sort<S>::quick(vector<S> &arr, long low, long high) {
 	
 	if( low < high ) {
 		
-		int pidx = partition(arr, low, high);
+		long pidx = partition(arr, low, high);
 		
 		quick(arr, low, pidx - 1); //left of pivot
 		quick(arr, pidx + 1, high); //right of pivot
@@ -224,8 +254,9 @@ void Sort::quick(vector<DTYPE> &arr, int low, int high) {
 	
 }
 
-void Sort::quickSort(bool decr=false) {
-	vector<DTYPE> daa = this->data;
+template<typename S>
+void Sort<S>::quickSort(bool decr) {
+	vector<S> daa = this->data;
 	
 	quick(daa, 0, daa.size()-1);
 	
@@ -245,25 +276,24 @@ int main()
 	//  float, etc.) (take a look at the for loop
 	// below)
 	
-    long int length;
-	vector<DTYPE> v;
-    
-    long int max = 10000, min = -50;
-	
-	srand(time(0));
-    
-    cout<<"Enter length of list:- "; cin>>length;
-    for(int i=0; i<length; i++) {
-    	DTYPE var = (rand()%max + min)*floatGEN;
-    	v.push_back(var);
-    }
+    long length;
+	vector<int> v;
 
-    Sort s(v);
+    Sort<int> s;
+    s.addData(10);
+    s.addData(1);
+    s.addData(-1);
+    s.addData(21);
+    s.addData(5);
+    s.addData(7);
+    s.addData(10);
+    s.addData(-5);
+
     s.display();
+    s.changeData(0, 2);
     cout<<endl;
     s.quickSort();
     s.display();
-    
     cout<<"\n\n\n";
     
     return 0;
